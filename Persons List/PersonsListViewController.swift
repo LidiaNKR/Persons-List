60926c3d9b3e41ca073392e18e49cbc222f031a6
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-02-16 23:13:05 +0300

    SetupMainStoryBaorad

--- Persons List/PersonsListViewController.swift
+++ Persons List/PersonsListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PersonsListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Persons List/SectionPersonalListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PersonalListSectionViewController.swift
+//  Persons List
+//
+//  Created by Лидия Ладанюк on 16.02.2021.
+//
+
+import UIKit
+
+class SectionPersonalListViewController: UITableViewController {
+    
+//    var personList = Person.getPersonInfo()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        personList.count
+//    }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Person", for: IndexPath)
+////
+//        let person = personList[indexPath.row]
+//
+//        var content = cell.defaultContentConfiguration()
+//        content.name = person.name
+//        content.surname = person.surname
+//
+//        cell.contentConfiguration = content
+//
+//        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+
+
