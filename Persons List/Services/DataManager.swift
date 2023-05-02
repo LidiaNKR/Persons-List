6c84fad2ca9eadbd968790f3f618ec32e93775fb
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2023-05-02 16:58:31 +0300

    Setup model layer

--- Persons List/Services/DataManager.swift
+++ Persons List/Services/DataManager.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    var names = ["Mila", "Jo", "Tom", "Rob",
+                "Rosa", "Sharon", "Melisa",
+                "Nicol", "Ted", "Sam"]
+    
+    var surnames = ["Smith", "Bolduin", "Ford", "Fox",
+                "Steard", "Dilan", "Farel",
+                "Uillis", "Delon", "Robbi"]
+    
+    var emails = ["11@mail.ru", "22@gmail.com", "33@mail.ru", "44@gmail.com",
+                "55@gmail.com", "66@mail.ru", "77@gmail.com",
+                "88@mail.ru", "88@gmail.com", "10@mail.ru"]
+    
+    var phones = ["12345", "54321", "354636", "575567856",
+                "34643", "27367", "245756",
+                "2476", "276556", "457678"]
+    
+    private init() {}
+}
