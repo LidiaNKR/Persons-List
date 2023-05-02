e553bb86143141818998f9c9d6c53e25dd6beca7
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2023-05-02 17:21:20 +0300

    Setup PersonListVC

--- Persons List/PersonListTableViewController.swift
+++ Persons List/PersonListTableViewController.swift
@@ -11,12 +11,29 @@ class PersonListTableViewController: UITableViewController {
     
     // MARK: - Public Properties
     var persons: [Person] = []
-
-    // MARK: - Life Cycles Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        persons.count
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let person = persons[indexPath.row]
+        
+        content.text = person.fullName
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = tableView.indexPathForSelectedRow {
+            let detailVC = segue.destination as! PersonDetailViewController
+            detailVC.person = persons[indexPath.row]
+        }
+    }
 }
 
